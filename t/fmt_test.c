5e08f483543f1df8f4a0c93eb400b2207a8e62ef
Matthew Zito <exbotanical@protonmail.com>
2023-07-04 10:49:20 -0700

    fix(fmt): fix valgrind errors in unit test

--- t/fmt_test.c
+++ t/fmt_test.c
@@ -4,6 +4,8 @@
 void test_fmt_str(void) {
   char *formatted = fmt_str("%s %d %s", "test", 11, "string");
   is(formatted, "test 11 string", "formats each part into a single string");
+
+  free(formatted);
 }
 
 int main() {
